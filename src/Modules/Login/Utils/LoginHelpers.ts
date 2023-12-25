75b311009abbd4db50821ba66b16d183d519a8b2
m-qm <41054106+m-qm@users.noreply.github.com>
2023-12-25 03:10:30 +0100

    Update LoginHelpers.ts

--- src/Modules/Login/Utils/LoginHelpers.ts
+++ src/Modules/Login/Utils/LoginHelpers.ts
@@ -2,8 +2,8 @@ import * as Yup from 'yup';
 import { LoginInitialValuesInterface } from './LoginInterfaces';
 
 export const LoginValidationSchema = Yup.object().shape({
-  email: Yup.string().email("Invalid email").required("Required"),
-  password: Yup.string().required("No password provided."),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('No password provided.'),
 });
 
 export const LoginInitialValues: LoginInitialValuesInterface = {
